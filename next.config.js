897fd5ffb898a1ea04bd390d559889f4a4924928
haruki0409 <76884995+haruki0409@users.noreply.github.com>
2023-08-21 13:02:07 +0900

    ran prettier

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/GDSC-web',
-  assetPrefix: '/GDSC-web/',
+  basePath: "/GDSC-web",
+  assetPrefix: "/GDSC-web/",
 };
 
 module.exports = {
