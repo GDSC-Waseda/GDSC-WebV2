8fa446e353ba7716e37722f266b189b1824400ff
lahiru <64465924+s3nmith@users.noreply.github.com>
2023-08-26 00:03:54 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/GDSC-web/public/",
+  basePath: "/GDSC-web/",
   assetPrefix: "/GDSC-web/",
 };
 
