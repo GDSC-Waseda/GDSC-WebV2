680dbc0b2b43c42b9d7e1aa06b808a01fee4c181
s3nmith <lahiruudawatta@gmail.com>
2023-11-17 16:42:50 +0900

    Loader Screen

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+// components/Loading.tsx
+
+const Loading = () => {
+  return (
+    <div id="loader-wrapper">
+      <img src="/loader.jpg" alt="Loading..." id="loader-image" />
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,35 @@
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Router from "next/router";
 
 import { Footer } from "components/Footer";
 import { NavigationBar } from "components/NavigationBar";
-import { googleTagManagerId } from "~/api/gtm";
-
+import Loading from "../components/Loading"; // Import the Loading component
 import "styles/globals.scss";
 
-/*import GoogleTagManager, {
-  GoogleTagManagerId
-} from 'components/GoogleAnalytics'*/
-
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => setLoading(true);
+    const end = () => setLoading(false);
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
 
   return (
     <>
-      {/*<GoogleTagManager
-        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
-  /> */}
+      {loading && <Loading />}{" "}
+      {/* Display the loading component when loading */}
       <NavigationBar />
       <Component {...pageProps} />
       <Footer />
